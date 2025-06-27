8d099942f1998a52a1183b763cf482bad6dfdf51
D191-AnggitaRamadanisBrilliano <anggitaramo@gmail.com>
2025-06-27 14:51:55 +0700

    Update studykasus.cpp

--- p.cpp
+++ p.cpp
@@ -2,146 +2,125 @@
 #include <iomanip>
 using namespace std;
 
-class Siswa {
-public:
-    char nim[20];
-    float nilai1;
-    float nilai2; 
-    float nilai3;
+struct Mahasiswa {
+
+    string nim;
+    float nilai[3];
     float rataRata;
     
     void hitungRataRata() {
-        rataRata = (nilai1 + nilai2 + nilai3) / 3;
+        rataRata = (nilai[0] + nilai[1] + nilai[2]) / 3;
     }
 };
 
 void tampilkanMenu() {
-    cout << "=========MENU======" << endl;
-    cout << "1. Input Nilai Siswa           " << endl;
-    cout << "2. Tampilkan Tabel Nilai       " << endl;
-    cout << "3. Sorting Rata-rata Ascending " << endl;
-    cout << "4. Sorting Rata-rata Descending" << endl;
-    cout << "5. Keluar                      " << endl;
-    cout << "Masukkan Pilihan : ";
+    cout << "===== MENU =====\n";
+    cout << "1. Input Nilai Siswa\n";
+    cout << "2. Tampilkan Tabel Nilai dan Rata-rata\n";
+    cout << "3. Sorting Rata-rata (Ascending)\n";
+    cout << "4. Sorting Rata-rata (Descending)\n";
+    cout << "5. Keluar\n";
+    cout << "Pilih menu: ";
 }
 
-void inputNilai(Siswa siswa[], int &jumlah) {
-    
+void inputNilai(Mahasiswa mahasiswa[], int &jumlah) {
+    cout << "Masukkan jumlah mahasiswa : ";
+    cin >> jumlah;
+    cout << "Masukkan nilai untuk " << jumlah << " siswa (3 mata pelajaran):\n";
     for (int i = 0; i < jumlah; i++) {
-    	cout << "siswa"<<i+1 <<":"<<endl;
-        cout << "Masukkan NIM " <<  " : ";
-        cin >> siswa[i].nim;
-        cout << "Input Nilai mapel 1 : ";
-        cin >> siswa[i].nilai1;
-        cout << "Input Nilai mapel 2 : ";
-        cin >> siswa[i].nilai2;
-        cout << "Input Nilai mapel 3 : ";
-        cin >> siswa[i].nilai3;
-       
-        
-        siswa[i].hitungRataRata();
+        cout << "Siswa " << (i + 1) << ":\n";
+        cout << "Nim " << (i + 1) << " : ";
+        cin >> mahasiswa[i].nim;
+        cout << "Nilai mapel 1: ";
+        cin >> mahasiswa[i].nilai[0];
+        cout << "Nilai mapel 2: ";
+        cin >> mahasiswa[i].nilai[1];
+        cout << "Nilai mapel 3: ";
+        cin >> mahasiswa[i].nilai[2];
+        mahasiswa[i].hitungRataRata();
     }
-    
-    cout << "  NILAI SISWA BERHASIL DIINPUT      " << endl;
-
 }
 
-void tampilkanTabel(Siswa siswa[], int jumlah) {
-
-    cout << "\n+==============================================================+" << endl;
-    cout << "| No |    NIM     |   M1   |   M2   |   M3   |   Rata-rata   |" << endl;
-    cout << "+----+------------+--------+--------+--------+---------------+" << endl;
-    
+void tampilkanTabel(Mahasiswa mahasiswa[], int jumlah) {
+    cout << "+-------------------------------------+\n";
+    cout << "| Siswa | Nim | M1  | M2  | M3 | Rata |\n";
+    cout << "+-------------------------------------+\n";
     for (int i = 0; i < jumlah; i++) {
-        cout << "| " << setw(2) << (i+1) << " | " 
-             << setw(10) << siswa[i].nim << " | "
-             << setw(6) << fixed << setprecision(1) << siswa[i].nilai1 << " | "
-             << setw(6) << fixed << setprecision(1) << siswa[i].nilai2 << " | "
-             << setw(6) << fixed << setprecision(1) << siswa[i].nilai3 << " | "
-             << setw(13) << fixed << setprecision(6) << siswa[i].rataRata << " |" << endl;
+        cout << "| " << setw(5) << (i + 1) << " | " << setw(3) << mahasiswa[i].nim 
+             << " | " << setw(3) << (int)mahasiswa[i].nilai[0] 
+             << " | " << setw(3) << (int)mahasiswa[i].nilai[1] 
+             << " | " << setw(2) << (int)mahasiswa[i].nilai[2] 
+             << " | " << setw(4) << mahasiswa[i].rataRata << " |\n";
     }
-    cout << "+----+------------+---------+--------+--------+--------------+" << endl;
-    
-    float totalRataRata = 0;
-    for (int i = 0; i < jumlah; i++) {
-        totalRataRata += siswa[i].rataRata;
-    }
-    
-    
+    cout << "+-------------------------------------+\n";
 }
 
-void bubbleSortAscending(Siswa siswa[], int jumlah) {
+void sortingAscending(Mahasiswa mahasiswa[], int jumlah) {
     for (int i = 0; i < jumlah - 1; i++) {
-        for (int j = 0; j < jumlah - i - 1; j++) {
-            if (siswa[j].rataRata > siswa[j + 1].rataRata) {
-                Siswa temp = siswa[j];
-                siswa[j] = siswa[j + 1];
-                siswa[j + 1] = temp;
+        for (int j = i + 1; j < jumlah; j++) {
+            if (mahasiswa[i].rataRata > mahasiswa[j].rataRata) {
+                Mahasiswa temp = mahasiswa[i];
+                mahasiswa[i] = mahasiswa[j];
+                mahasiswa[j] = temp;
             }
         }
     }
-    cout << "Data berhasil diurutkan secara Ascending (terendah ke tertinggi)! " << endl;
+
+    cout << "Rata-rata siswa (Ascending): ";
+    for (int i = 0; i < jumlah; i++) {
+        cout << mahasiswa[i].rataRata;
+        if (i < jumlah - 1) cout << " | ";
+    }
+    cout << " |\n";
 }
 
-void bubbleSortDescending(Siswa siswa[], int jumlah) {
+void sortingDescending(Mahasiswa mahasiswa[], int jumlah) {
     for (int i = 0; i < jumlah - 1; i++) {
-        for (int j = 0; j < jumlah - i - 1; j++) {
-            if (siswa[j].rataRata < siswa[j + 1].rataRata) {
-                Siswa temp = siswa[j];
-                siswa[j] = siswa[j + 1];
-                siswa[j + 1] = temp;
+        for (int j = i + 1; j < jumlah; j++) {
+            if (mahasiswa[i].rataRata < mahasiswa[j].rataRata) {
+                Mahasiswa temp = mahasiswa[i];
+                mahasiswa[i] = mahasiswa[j];
+                mahasiswa[j] = temp;
             }
         }
     }
-    cout << "Data berhasil diurutkan secara Descending (tertinggi ke terendah)" << endl;
+
+    cout << "Rata-rata siswa (Descending): ";
+    for (int i = 0; i < jumlah; i++) {
+        cout << mahasiswa[i].rataRata;
+        if (i < jumlah - 1) cout << " | ";
+    }
+    cout << " |\n";
 }
 
 int main() {
-    int jumlahSiswa;
-    cout << "Masukkan jumlah Siswa : ";
-    cin >> jumlahSiswa;
-    
-    Siswa siswa[jumlahSiswa];
+    const int MAX_MAHASISWA = 100;
+    Mahasiswa mahasiswa[MAX_MAHASISWA];
+    int jumlahMahasiswa = 0;
     int pilihan;
-    bool dataInputted = false;
-    
+
     do {
         tampilkanMenu();
         cin >> pilihan;
-        
+
         switch (pilihan) {
             case 1:
-                inputNilai(siswa, jumlahSiswa);
-                dataInputted = true;
+                inputNilai(mahasiswa, jumlahMahasiswa);
                 break;
             case 2:
-                if (dataInputted) {
-                    tampilkanTabel(siswa, jumlahSiswa);
-                } else {
-                    cout << "Belum ada data! Silakan input nilai terlebih dahulu. " << endl;
-                }
+                tampilkanTabel(mahasiswa, jumlahMahasiswa);
                 break;
             case 3:
-                if (dataInputted) {
-                    bubbleSortAscending(siswa, jumlahSiswa);
-                    tampilkanTabel(siswa, jumlahSiswa);
-                } else {
-                    cout << "Belum ada data! Silakan input nilai terlebih dahulu. " << endl;
-                }
+                sortingAscending(mahasiswa, jumlahMahasiswa);
                 break;
             case 4:
-                if (dataInputted) {
-                    bubbleSortDescending(siswa, jumlahSiswa);
-                    tampilkanTabel(siswa, jumlahSiswa);
-                } else {
-                    cout << "Belum ada data! Silakan input nilai terlebih dahulu. " << endl;
-                }
+                sortingDescending(mahasiswa, jumlahMahasiswa);
                 break;
             case 5:
-                cout << "Terima Kasih!" << endl;
+                cout << "Terima Kasih\n";
                 break;
             default:
-                cout << "Pilihan tidak valid!" << endl;
+                cout << "Pilihan tidak valid!\n";
         }
     } while (pilihan != 5);
     
